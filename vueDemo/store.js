379da76c283972a073f2f1b5bf45aebca219e9cb
苍穹勇敢的心 <18825237023@163.com>
2019-04-08 22:36:22 +0800

    vue store之状态管理模式

---
+++ vueDemo/store.js
@@ -0,0 +1,50 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+
+const store = new Vuex.Store({
+  state: {
+    count: 5,
+		loginUser:'',
+		isLogin: false,
+		openedTagNavList:[],
+		NavList:[]
+  },
+   // 更改store的状态
+  mutations: {
+    increment (state) {
+      state.count++
+    },
+    decrement (state) {
+      state.count--
+    },
+		setloginUser(state,val){
+			state.loginUser = val;
+			state.isLogin = true;
+		},
+		removeloginUser(state){
+			state.loginUser = '';
+			state.isLogin = false;
+		},
+		addNavList(state,val){
+			state.openedTagNavList.push(val);
+		},
+		removeTagNav(state,val){
+			var index = state.openedTagNavList.indexOf(val);
+			state.openedTagNavList.splice(index,1);
+		},
+		setAllNavList(state,val){
+			state.NavList.push(val);
+		},
+  },
+  // 通过getter 进行数据获取
+  // getters: {
+  //   getState(state) {
+  //     return state.count > 0 ? state.count : 0;
+  //   }
+  // }
+})
+
+export default store
