5c83315e09e1aaba33227b186c3000af87a9c519
苍穹勇敢的心 <18825237023@163.com>
2019-05-30 15:20:29 +0800

    多语言

---
+++ iSales/lang/en.js
@@ -0,0 +1,112 @@
+export default {
+  navbar: {
+    logOut: 'Log Out',
+    dashboard: 'Dashboard',
+    github: 'Github',
+    screenfull: 'Screenfull',
+    theme: 'Theme',
+    size: 'Global Size',
+    loginTime: 'Login Time'
+  },
+  login: {
+    title: 'Login Form',
+    logIn: 'Log in',
+    username: 'Username',
+    password: 'Password',
+    any: 'any',
+    thirdparty: 'Or connect with',
+    thirdpartyTips: 'Can not be simulated on local, so please combine you own business simulation! ! !'
+  },
+  tagsView: {
+    refresh: 'Refresh',
+    close: 'Close',
+    closeOthers: 'Close Others',
+    closeAll: 'Close All'
+  },
+  mideaMessage: {
+    defaultErrorInfo: 'error request, please contact with the administrator!',
+    cancelButtonText: 'Cancel',
+    confirmButtonText: 'Confirm',
+    showMoreText: 'more'
+  },
+  common: {
+    noData: 'no data',
+    confirm: 'OK',
+    cancel: 'Cancel',
+    success: 'Success!',
+    add: 'New',
+    edit: 'Edit',
+    delete: 'Delete',
+    layout: 'Layout',
+    pleaseSelect: 'Select',
+    cannotEdit: 'Please select one you want to edit',
+    cannotDelete: 'Please select one you want to delete',
+    errorInput: 'Please enter correct',
+    errorSelect: 'Please select correct',
+    errorNumber: 'Please enter correct Number',
+    apply: 'Apply',
+    save: 'Save',
+    save_apply: 'Save&Apply',
+    layout_title: 'Layout',
+    tooLong: 'Too long, max characters:',
+    confirm_enable: 'Make them enable?',
+    confirm_disable: 'Make them disable?',
+    error: 'Error!',
+    pleaseTypeContents: 'please type contents',
+    editted: 'Save all you editted?',
+    checkEdit: 'Please check your input',
+    delRow: 'Delete row?',
+    export: 'Export',
+    fileName: 'File Name',
+    editBeforeSave: 'Please edit the row you Editting',
+    all: 'All',
+    setMin: 'Cannot uncheck all',
+    search: 'Search',
+    enable: 'Enable',
+    disable: 'Disable',
+    show: 'Show',
+    hide: 'Hide',
+    onceSave: 'onceSave',
+    deleteViews: 'This operation will delete data you checked from table, if you want to delete them from database, please click "save"',
+    cannotSave: 'There is not any data need be saved'
+  },
+  // 组件
+  components: {
+    headers: {
+      sort: 'Sort',
+      columnName: 'Column name',
+      hidden: 'Hide',
+      width: 'Width',
+      freeze: 'Freeze'
+    },
+    showFilter: 'Show filter',
+    left: 'Left',
+    right: 'Right',
+    none: 'None',
+    pager: {
+      showing: 'Showing',
+      to: 'to',
+      of: 'of',
+      entries: 'Entries'
+    },
+    condition: {
+      bt:'between',
+      cn: 'contians',
+      eq:'equal',
+      ne:'not equal',
+      me:'multi equal',
+      bw:'begins with',
+      ew:'ends with',
+      gt:'greater',
+      ge:'greater or equal',
+      lt:'less',
+      le:'less or equal'
+    },
+    fileupload: {
+      dragfile: 'Drag the file here, or',
+      clickupload: 'Click upload',
+      fileslimit: 'Files exceed the number limit', 
+      uploadFailed: 'Upload failure!'
+    }
+  }
+}
---
+++ iSales/lang/ja.js
@@ -0,0 +1,114 @@
+export default {
+  navbar: {
+    logOut: '退出登录',
+    dashboard: '首页',
+    github: '项目地址',
+    screenfull: '全屏',
+    theme: '换肤',
+    size: '布局大小',
+    loginTime: '登录时间'
+  },
+  login: {
+    title: '系统登录',
+    logIn: '登录',
+    username: '账号',
+    password: '密码',
+    any: '随便填',
+    thirdparty: '第三方登录',
+    thirdpartyTips: '本地不能模拟，请结合自己业务进行模拟！！！'
+  },
+  tagsView: {
+    refresh: '刷新',
+    close: '关闭',
+    closeOthers: '关闭其它',
+    closeAll: '关闭所有'
+  },
+  mideaMessage: {
+    defaultErrorInfo: '请求异常，请与管理员联系!',
+    cancelButtonText: '取消',
+    confirmButtonText: '確定',
+    showMoreText: '更多'
+  },
+  common: {
+    noData: '暂无数据',
+    confirm:'確定',
+    cancel:'取消',
+    success:' !',
+    add:'新規追加',
+    edit:'修正',
+    delete:'删除',
+    reset: '重置',
+    layout:'布局',
+    pleaseSelect:'お選び下さい',
+    cannotEdit:'修正したい項目をお選び下さい',
+    cannotDelete:'削除したい項目をお選び下さい',
+    errorInput:'内容を正しくご入力下さい',
+    errorSelect:'内容を正しくお選び下さい',
+    errorNumber:'数字をご入力下さい',
+    apply: '应用',
+    save: '登録',
+    save_apply:'应用并保存',
+    layout_title:'布局',
+    tooLong:'最大长度:',
+    confirm_enable:'设置为可用？',
+    confirm_disable:'禁用？',
+    error:'出错了!',
+    pleaseTypeContents:'内容をご入力下さい',
+    editted:'保存编辑内容?',
+    checkEdit:'请检查录入的内容',
+    delRow:'該当行を削除する?',
+    export:'导出',
+    fileName:'文件名',
+    editBeforeSave:'请编辑未保存的行',
+    all:'全部',
+    setMin:'设置了最少选择个数，不能取消全选',
+    search:'查询',
+    enable:'启用',
+    disable:'禁用',
+    show:'显示',
+    hide:'隐藏',
+    onceSave:'暂存',
+    deleteViews:'从界面删除选中内容，如果需要删除数据请点击保存',
+    cannotSave:'没有需要保存的内容'
+  },
+  // 组件
+  components: {
+    headers:{
+      sort:'排序',
+      columnName:'列名',
+      hidden:'隐藏',
+      width: '列宽',
+      freeze: '冻结',
+      operation: '操作',
+    },
+    showFilter:'启用过滤条',
+    left:'左',
+    right:'右',
+    none:'不冻结',
+    pager: {
+      showing: '当前显示',
+      to:'至',
+      of:'共',
+      entries:'条'
+    },
+    condition: {
+      bt:'之间',
+      cn: '包含',
+      eq:'等于',
+      ne:'不等',
+      me:'多选',
+      bw:'开始于',
+      ew:'结束于',
+      gt:'大于',
+      ge:'大于等于',
+      lt:'小于',
+      le:'小于等于'
+    },
+    fileupload:{
+      dragfile:'将文件拖到此处，或',
+      clickupload:'点击上传',
+      fileslimit:'文件超出个数限制', //Files exceed the number limit
+      uploadFailed:'上传失败!'
+    }
+  }
+}
