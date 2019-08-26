d6c6a6f5cadc8357e8b19091e4c13d7e13e51639
苍穹勇敢的心 <18825237023@163.com>
2019-08-26 11:18:33 +0800

    添加组件之间传值

--- nuxt-demo/pages/home/index.vue
+++ nuxt-demo/pages/home/index.vue
@@ -2,24 +2,33 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-08-13 17:34:08
- * @LastEditTime: 2019-08-14 10:00:48
+ * @LastEditTime: 2019-08-26 11:13:30
  * @LastEditors: Please set LastEditors
  -->
 <template>
   <div class="user">
     <div>
         <h2>home</h2>
+        <p><span>{{data3}}</span><button @click="getParam" class="btn btn-info">获取传值param</button></p>
     </div>
   </div>
 </template>
 
 <script>
+import {pageParamsMixin} from '@/utils/mixins';
 
 export default {
+  name:'homepage',
+   mixins:[pageParamsMixin],
   data() {
     return{
       
     }
+  },
+  methods:{
+     getParam(){
+      console.log('获取传值---', this.data3)
+    },
   }
 }
 </script>
