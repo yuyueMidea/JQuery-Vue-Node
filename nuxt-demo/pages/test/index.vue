ad639a549503a4f932b52d46da952acf9695985b
苍穹勇敢的心 <18825237023@163.com>
2019-08-26 11:19:05 +0800

    添加组件之间传值

--- nuxt-demo/pages/test/index.vue
+++ nuxt-demo/pages/test/index.vue
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-08-13 17:34:08
- * @LastEditTime: 2019-08-14 13:56:20
+ * @LastEditTime: 2019-08-26 11:15:16
  * @LastEditors: Please set LastEditors
  -->
 <template>
@@ -10,6 +10,7 @@
     <div>
         <h2>test</h2>
         <p>
+          <button class="btn btn-info" @click="setval">设置传值Param</button>
           <button class="btn btn-info" @click="debu">断点测试</button>
           <button class="btn btn-info" @click="getData">测试接口</button>
         </p>
@@ -23,17 +24,27 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from 'axios';
+import {pageParamsMixin} from '@/utils/mixins';
 
 export default {
+  name:'testDemo',
+  mixins:[pageParamsMixin],
   data() {
     return{
       routesList:[],
     }
   },
   methods:{
+    setval(){
+       this.setParam('这是要传值')
+    },
     debu(){
-      debugger
+      this.$message({
+              type: 'info',
+              message: `这是一条消息`
+            });
+           
     },
     getData(){
         axios.get('https://jsonplaceholder.typicode.com/users')
